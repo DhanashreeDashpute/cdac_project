ff0f72362ba1f597861e015eee2fd53d600742e4
Dhanashree Dashpute <166618431+DhanashreeDashpute@users.noreply.github.com>
2024-08-15 09:48:06 +0530

    Delete PaymentRepository.java

--- PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecart.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecart.model.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
-
